d220785a849c6a3c6cb73c732f9edf1784fb4c6d
Vinayvadavi <137748631+Vinayvadavi@users.noreply.github.com>
2024-01-02 20:04:53 +0530

    Update one.py

--- one.py
+++ one.py
@@ -1,2 +1,14 @@
 GOOD MORNING ALL OF YOU....
 Good morning boys
+Hi I am having 7+rs of exp in wina&vmadmin
+and cuurne working in algonomy as serveradmin
+and here my responsibilities are 
+and am havign know on vcenter, windows server, linus
+server too
+and day to day activit
+installing and confiugu and maintai the server physical and 
+vm's and instances too
+and monitoring the server though tool
+and handling the serverice like AD dhcp dns backup service
+patch service monthly file server and NAS server too
+and 
